0d34bbf33c9744bb0928561e03feebef0b44c302
Padmanabh <prabhas.padmanabh@gmail.com>
2014-09-14 00:56:47 +0530

    separate json file

--- assets/js/maincontroller.js
+++ assets/js/maincontroller.js
@@ -1,29 +1,11 @@
-app.controller("MainController", function($scope){
+app.controller("MainController", function($scope, $http){
   $scope.selectedNoteId = 0;
   $scope.setActive = function(id) {
     $scope.selectedNoteId = id;
   };
-  $scope.data = {
-    "count": 2,
-    "title": "My Blog",
-    "author": "Padmanabh",
-    "notes": [
-        {
-        "id": 0,
-        "date": "1234",
-        "title": "Post no 1",
-        "tags": ["test","padmanabh","javascript"],
-        "content": "<p>this is the actual post</p>",
-        },
-        {
-        "id":1,
-        "date": "1235",
-        "title": "Post no 2",
-        "tags": ["test","jdfop","javascript"],
-        "content": "<p>this is the actual post no2</p>",
-        },
-    ]
-    };
-
+  $http.get('data.json').then(function(rest){
+    $scope.data = res.data
+  });
+  
 
 });
